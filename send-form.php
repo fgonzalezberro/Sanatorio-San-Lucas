7104ec20488e8d782d542b45e42de1e9037b7269
fgonzalezberro <Developer@MacBook-Air-de-int.local>
2020-04-08 09:09:31 -0300

    Heroku config

--- send-form.php
+++ send-form.php
@@ -13,8 +13,12 @@
    $emailMessage .= "Asunto: $affair \n";
    $emailMessage .= "Mensaje: $message";
 
+   // Aditional Headers
+   $aditionalHeaders = "From: $email \r\n";
+   $aditionalHeaders .= "Bcc: <fgonzalezberro@gmail.com> \r\n";
+
    //Send Email
-   $mail = @mail($select, $affair, $emailMessage);
+   $mail = @mail($select, $affair, $emailMessage, $aditionalHeaders);
 
    if($mail){
       echo '<div style= "height: 100vh; width:100%; display: flex; flex-direction: column; align-items:center; justify-content: center;"><p style="border: 2px solid #0c82c6; color: #0c82c6; padding: 5% 10%; margin-bottom: 2%;"> El email se envío correctamente</p> <a href="index.html"> <button style= "padding: 10px 20px; border: 2px solid #0c82c6; background-color: #0c82c6; color: #fff; cursor: pointer;">Volver al sitio</button></a></div>';

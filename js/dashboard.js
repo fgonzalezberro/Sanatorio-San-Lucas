1fbec52c5decb26c414ccae077fca86d73363bfc
fgonzalezberro <Developer@MacBook-Air-de-int.local>
2020-04-10 19:13:31 -0300

    Dashboard update, add news section styles

--- js/dashboard.js
+++ js/dashboard.js
@@ -36,4 +36,26 @@ $(document).ready(() =>{
     // Evaluate the state of the button
     return btnStatus == true ? openSidebar() : closeSidebar();
   });
+
+  // Show and Hide 'Dashboard' sections
+  const changeDashSection = (classSection) =>{
+    $(".dashboard-main").children('div , h2').hide();
+    $(".dashboard-main").css('padding' , '0');
+    $(classSection).css({
+      'display': 'flex',
+      'flex-direction' : 'column',
+      'justify-content' : 'center',
+      'align-items' : 'center'
+    });
+  }
+
+  $(".add-news-opt").click(() => changeDashSection('.add-news-section'));
 });
+
+// Open Input 'file'
+$(".charge-image-button").click(() =>{
+  $("#news-input-file").click();
+});
+
+// Datepicker function
+$(".news-date").datepicker(); // When the usr click in 'Date input' the Datepicker is Open

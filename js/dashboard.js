cebad961054ed39ff187a322f79d67b25835ea65
fgonzalezberro <Developer@MacBook-Air-de-int.local>
2020-04-12 23:50:00 -0300

    Add firebase database connection, Editor jQuery plugin and change Dashboard Styles

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,11 @@
 // Loading document
 $(document).ready(() =>{
+
   // Set the initial btn status
   let btnStatus = true;
+
+  // Transform 'News Textarea' in a Editable textarea
+  $(".news-content").Editor(); 
 
   // Set Sidebar animation's
   $(".dashboard-btn").click(() =>{
@@ -50,12 +54,68 @@ $(document).ready(() =>{
   }
 
   $(".add-news-opt").click(() => changeDashSection('.add-news-section'));
-});
 
-// Open Input 'file'
-$(".charge-image-button").click(() =>{
-  $("#news-input-file").click();
-});
+  // Open Input 'file' when the usr click the element 'Charge Image Button'
+  $(".charge-image-button").click(() =>{
+    $("#news-input-file").click();
+  });
 
-// Datepicker function
-$(".news-date").datepicker(); // When the usr click in 'Date input' the Datepicker is Open
+  // Datepicker function
+  $(".news-date").datepicker(); // When the usr click in 'Date input' the Datepicker is Open
+
+  // Capture the Input Type element
+  const inputFileType = document.getElementById('news-input-file');
+
+  // This function upload all data in firebase DB
+  $(".add-new-btn").click(() =>{
+    // Prevent Default Action
+    $("form").submit((e) =>{
+      e.preventDefault();
+    });
+
+   // Set Firebase References
+   let storageRef = firebase.storage().ref();
+   let dataBaseRef = firebase.database().ref();
+
+   // Create a node in data base with the inputs info
+   const createNodeInDataBase = (imageName, imageURL) =>{
+     let newsTitle = $(".news-title").val();
+     let newsDate = $(".news-date").val();
+     let newsContent = $(".news-content").Editor('getText');
+
+     dataBaseRef.push({
+       name: imageName,
+       url: imageURL,
+       title: newsTitle,
+       date: newsDate,
+       content: newsContent
+     });
+   }
+
+   // Define the image to upload in the storage
+   const imageToUpload = inputFileType.files[0];
+   // Upload Image
+   let uploadTask = storageRef.child('newsImages/' + imageToUpload.name).put(imageToUpload);
+
+    // Upload storage handler
+    uploadTask.on('state_changed', function(snapshot){
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+        console.log(error);
+    }, function() {
+      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        createNodeInDataBase(imageToUpload.name , downloadURL);
+      });
+    });
+  });
+});
